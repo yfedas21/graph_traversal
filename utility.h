e80468a6f153833d746fab27e35a8c0b78a80cc6
Yuriy V Fedas <yfedas21@my.whitworth.edu>
2019-12-12 11:06:21 -0800

    changed structure, arrays using heap instead of stack

--- utility.h
+++ utility.h
@@ -4,8 +4,11 @@
 #include <time.h>   // seed based on time
 #include <array>	// vector unnecessary because of static size
 
-	// define the graph sizes as constants
-	// to generate 2D arrays
+#include <algorithm> 
+#include <ctime>	// for timing 
+
+// define the graph sizes as constants
+// to generate 2D arrays
 #define TEN		10
 #define HUNDRED 100
 #define THOUSAND 1000
@@ -20,9 +23,24 @@ namespace utility {
 		return(++number);
 	}
 
+	// function to create an n x n graph using 1-D array
+	// @inputs graph_loc: mem addr of first element 
+	//		   SIZE: number of elements in array
+	// @return the mem addr of graph (first element of array)
+	void populate_graph(int* graph_loc, int SIZE) {
+		for (int i = 0; i < SIZE; ++i) {
+			for (int j = i; j < SIZE; ++j) {
+				if (j == i)
+					graph_loc[i * SIZE + j] = 0;
+				else {
+					graph_loc[i * SIZE + j] = graph_loc[j * SIZE + i] = rand() % 24 + 1;
+				}
+			}
+		}
+	}
+
 	// print an array of any size
-	template<std::size_t SIZE>
-	void print_matrix(std::array<int, SIZE> arr, int LEN) {
+	void print_graph(int* arr, int LEN) {
 		// debug function; see whats in the array
 		//for (int i = 0; i < ten_graph.size(); ++i)
 		//	std::cout << ten_graph[i] << std::endl;
@@ -37,8 +55,7 @@ namespace utility {
 	}
 
 	// print upper triangular including main diagonal 
-	template<std::size_t SIZE>
-	void print_upper_t(std::array<int, SIZE> arr, int LEN) {
+	void print_upper_t(int* arr, int LEN) {
 		// debug function; see whats in the array
 		//for (int i = 0; i < ten_graph.size(); ++i)
 		//	std::cout << ten_graph[i] << std::endl;
@@ -52,26 +69,11 @@ namespace utility {
 		}
 	}
 
-	// function to input random data into graphs of arbitrary size
-	template<std::size_t SIZE>
-	void input_rand_data(std::array<int, SIZE> arr, int LEN) {
-		// use rand() % 25 to get weights between 0 to 25
-		for (int i = 0; i < LEN; ++i) {
-			std::cout << "< ";
-			for (int j = i; j < LEN; ++j)
-				arr[i * LEN + j] = rand() % 25;
-			std::cout << ">" << std::endl;
-		}
-	}
-
 	// function to run algorithm comparisons
 	void run_algs()
 	{
 		// STEP 1: GENERATE GRAPH
-		// 10 ROWS by 10 COLUMNS graph  (PAY ATTENTION TO ROW/COL ORDER!!!)
-		//				ROW * COL
-		std::array<int, TEN * TEN> ten_graph;
-		
+
 		// IMPORTANT TO UNDERSTAND
 		// (rows and columns start from 1. So, for example, 
 		// if you want to get the value 7, its in the 1st row, 
@@ -79,57 +81,42 @@ namespace utility {
 		// it is 0-indexed. So graphically, its the first row of a table, but 
 		// in code, its the 0th row), use 
 
-		// reason why the graphs were generated here was because 
-		// didn't want to deal with passing large arrays between functions
-		// generate random data for 10 x 10 matrix
-		for (int i = 0; i < TEN; ++i) {
-			for (int j = i; j < TEN; ++j)
-				if (j == i) 
-					ten_graph[i * TEN + j] = 0;
-				else {
-					ten_graph[i * TEN + j] = ten_graph[j * TEN + i] = rand() % 25;
-				}
-					
-		}
+		// 10 x 10 graph; remember to deallocate!
+		int* ten_graph;
+		ten_graph = new int[TEN * TEN];
+
+		// populate the graph with random values (complete graph) 
+		populate_graph(ten_graph, TEN); 
+
+
+		// 100 x 100 graph; remember to deallocate!
+		int* hun_graph;
+		hun_graph = new int[HUNDRED * HUNDRED];
+
+		populate_graph(hun_graph, HUNDRED);
+
+		// 1000 x 1000 graph; remember to deallocate!
+		int* tho_graph;
+		tho_graph = new int[THOUSAND * THOUSAND];
+
+		populate_graph(tho_graph, THOUSAND);
 
 		// EXAMPLE: HOW TO VIEW GRAPH AS 2D array
 		// print the 10 x 10 matrix for debugging
-		print_matrix(ten_graph, 10); 
+		print_graph(ten_graph, 10);
 
-		// create 100 x 100 matrix
-		std::array<int, HUNDRED * HUNDRED> hun_graph;
-
-		// generate adjacencies with semi-random numbers
-		for (int i = 0; i < HUNDRED; ++i) {
-			for (int j = i; j < HUNDRED; ++j)
-				if (j == i)
-					hun_graph[i * HUNDRED + j] = 0;
-				else {
-					hun_graph[i * HUNDRED + j] = hun_graph[j * HUNDRED + i] = rand() % 25;
-				}
-
-		}
-
-		// create 1000 x 1000 matrix
-		std::array<int, THOUSAND * THOUSAND> tho_graph;
-
-		// generate adjacencies with semi-random numbers
-		for (int i = 0; i < THOUSAND; ++i) {
-			for (int j = i; j < THOUSAND; ++j)
-				if (j == i)
-					tho_graph[i * THOUSAND + j] = 0;
-				else {
-					tho_graph[i * THOUSAND + j] = tho_graph[j * THOUSAND + i] = rand() % 25;
-				}
-
-		}
 
 		// EXAMPLE: access the graphs
 		// the 10 x 10: ten_graph
 		// 				hun_graph
 		//				tho_graph
-		
-		// EXAMPLE: access the 5th element of the 7th row: (assert: value = 64)
+
+		// EXAMPLE: access the 5th element of the 7th row:
 		std::cout << ten_graph[6 * TEN + 4] << std::endl;
+
+		// deallocate memory
+		delete[] ten_graph; 
+		delete[] hun_graph; 
+		delete[] tho_graph; 
 	}
 }
